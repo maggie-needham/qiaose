25c25b57eb06f6064a42f1f409d8108da0d4812d
maggie-needham <maggieneedham01@gmail.com>
2025-10-21 13:44:56 -0400

    experimenting with adding animation; found bugs with format on google chrome vs arc

---
+++ src.js
@@ -0,0 +1,7 @@
+// this function will bounce the given element around
+function move_a_lil (id){
+    console.log("in function")
+    const element = document.getElementById(id);
+    element.classList.add('animate__animated', 'animate__jello');
+    element.style.setProperty('--animate-duration', '2s');       
+}
