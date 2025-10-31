89931ec305b2c17986f189de00acfb264ff7406e
maggie-needham <maggieneedham01@gmail.com>
2025-10-31 12:12:19 -0400

    style changes that were lost

--- src.js
+++ src.js
@@ -3,5 +3,5 @@ function move_a_lil (id){
     console.log("in function")
     const element = document.getElementById(id);
     element.classList.add('animate__animated', 'animate__jello');
-    element.style.setProperty('--animate-duration', '2s');       
+    element.style.setProperty('--animate-duration', 2s);       
 }
