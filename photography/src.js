c68511d436b9eda4ef26f9f0b34b2f1a7f07f607
maggie-needham <maggieneedham01@gmail.com>
2025-11-09 15:08:29 -0500

    initial arrow + photo animation

---
+++ photography/src.js
@@ -0,0 +1,24 @@
+// get the collapsible elements
+var coll = document.getElementsByClassName("collapsible");
+var i;
+// set argentina as active
+var arg = document.getElementById("arg");
+arg.classList.toggle("active");
+// get the arrow icon 
+var arrow = document.getElementById("arrow");
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (this.classList.contains("active")) {
+        content.style.opacity = "1";
+        content.style.visibility = "visible";
+        arrow.style.rotate = "90deg";
+    } else {
+      content.style.opacity = "0";
+      content.style.visibility = "collapse";
+      arrow.style.rotate = "0deg";
+    }
+  });
+}
